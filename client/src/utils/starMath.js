e20a217eb2c4eb2e9a7d505efdbbc6f35d1e9885
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2023-10-08 18:20:36 -0400

    Star component and supporting utils, simplification of LOGIN query

---
+++ client/src/utils/starMath.js
@@ -0,0 +1,9 @@
+function parseRightAsc(ascString) {
+    return 5.0;  // TODO - dummy implementation
+}
+
+function numericRightAscToString(numAsc) {
+    return '23h50m23.570s';  // TODO - dummy implementation
+}
+
+export { parseRightAsc, numericRightAscToString }
