a33b0dbb7e8847f8c309df13eaa359f4c9da3f90
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2023-10-08 22:21:24 -0400

    disable direct edits on Star component, enable link to SingleStar, skeleton version of SingleStar (incomplete)

--- client/src/components/Star/index.jsx
+++ client/src/components/Star/index.jsx
@@ -90,47 +90,36 @@ function Star(props) {
       <h3>Some Star Information ... </h3>  {/*TODO*/}
 
       {isEditable ? (
-        <>
-          <form
-            className="flex-row justify-center justify-space-between-md align-center"
-            onSubmit={handleFormSubmit}
-          >
-            <div className="col-12 col-lg-9">
+        <Link to={`/stars/${props.star._id}`}>
+        <div className="star-readonly-outer">
+            <div className="star-readonly col-12 col-lg-9">
                 <table>
                     <tbody>
-                    <tr><td className="star-stat-label">Star Name</td><td><input name="starName" type="text" value={props.star.starName} onChange={handleChange} /></td></tr>
-                    <tr><td className="star-stat-label">Declination (degrees north)</td><td><input name="declination"  value={props.star.declination} onChange={handleChange} /></td></tr>
-                    <tr><td className="star-stat-label">Right Ascension</td><td><input name="rightAscension" value={props.star.rightAscension} onChange={handleChange} /></td></tr>
-                    <tr><td className="star-stat-label">Distance from Earth (light years)</td><td><input name="distanceFromEarth" value={props.star.distanceFromEarth} onChange={handleChange} /></td></tr>
+                    <tr><td className="star-stat-label">Star Name</td><td className="star-stat-val">{props.star.starName}</td></tr>
+                    <tr><td className="star-stat-label">Declination (degrees north)</td><td className="star-stat-val">{props.star.declination}</td></tr>
+                    <tr><td className="star-stat-label">Right Ascension</td><td className="star-stat-val">{props.star.rightAscension}</td></tr>
+                    <tr><td className="star-stat-label">Distance from Earth (light years)</td><td className="star-stat-val">{props.star.distanceFromEarth}</td></tr>
+                    <tr><td className="star-stat-label">Planet Count</td><td className="star-stat-val">{props.star.planets.length}</td></tr>
                     </tbody>
                 </table>
-            </div>
-
-            <div className="col-12 col-lg-3">
-              <button className="btn btn-primary btn-block py-3" type="submit">
-                Submit Star Data Changes
-              </button>
             </div>
             {error && (
               <div className="col-12 my-3 bg-danger text-white p-3">
                 {error.message}
               </div>
             )}
-          </form>
-        </>
+        </div>
+        </Link>
       ) : (
-        <>
-          <form
-            className="flex-row justify-center justify-space-between-md align-center"
-            onSubmit={handleFormSubmit}
-          >
-            <div className="col-12 col-lg-9">
+        <div className="star-editable-outer">
+            <div className="star-editable col-12 col-lg-9">
                 <table>
                     <tbody>
-                    <tr><td className="star-stat-label">Star Name</td><td>{props.star.starName}</td></tr>
-                    <tr><td className="star-stat-label">Declination (degrees north)</td><td>{props.star.declination}</td></tr>
-                    <tr><td className="star-stat-label">Right Ascension</td><td>{props.star.rightAscension}</td></tr>
-                    <tr><td className="star-stat-label">Distance from Earth (light years)</td><td>{props.star.distanceFromEarth}</td></tr>
+                    <tr><td className="star-stat-label">Star Name</td><td className="star-stat-val">{props.star.starName}</td></tr>
+                    <tr><td className="star-stat-label">Declination (degrees north)</td><td className="star-stat-val">{props.star.declination}</td></tr>
+                    <tr><td className="star-stat-label">Right Ascension</td><td className="star-stat-val">{props.star.rightAscension}</td></tr>
+                    <tr><td className="star-stat-label">Distance from Earth (light years)</td><td className="star-stat-val">{props.star.distanceFromEarth}</td></tr>
+                    <tr><td className="star-stat-label">Planet Count</td><td className="star-stat-val">{props.star.planets.length}</td></tr>
                     </tbody>
                 </table>
             </div>
@@ -139,8 +128,7 @@ function Star(props) {
                 {error.message}
               </div>
             )}
-          </form>
-        </>
+        </div>
       )}
     </div>
   );
